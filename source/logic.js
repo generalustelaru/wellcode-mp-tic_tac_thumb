8ae528606ea4db7e9db0ca018128d27de1d5ec1c
generalustelaru <57430865+generalustelaru@users.noreply.github.com>
2021-06-17 17:38:22 +0300

    Commented currently redundant lines
    i'm saving them though. I might implement bigger boards and different winning conditions.

--- source/logic.js
+++ source/logic.js
@@ -1,21 +1,21 @@
-var cellSize = 168, boardSize = 3, totalTurns = boardSize * boardSize;  // cellSize and style settings involving it (width, height) are experimental.
+var cellSize = 168, boardSize = 3, totalTurns = boardSize * boardSize;  // cellSize and style settings that use it (width, height) are experimental.
 var gameState = false, gameTurn, activePlayer;                          // You can change boardSize and use cellSize to fit different grids on your screen.
 var cellMonitor = new Map(); // Used to store and check cell states     // The game works in any configuration.
 function drawBoard() {
     let board = document.getElementById("gameBoard");
-    board.style.height = (cellSize * boardSize) + "px";
-    board.style.width = (cellSize * boardSize) + "px";
+    //board.style.height = (cellSize * boardSize) + "px";
+    //board.style.width = (cellSize * boardSize) + "px";
     for (let x = 1; x <= boardSize; x++) {
         let row = document.createElement("div");
         row.className = "row";
-        row.style.height = cellSize + "px";
-        row.style.width = (cellSize * boardSize) + "px";
+        //row.style.height = cellSize + "px";
+        //row.style.width = (cellSize * boardSize) + "px";
         for (let y = 1; y <= boardSize; y++) {
             let cell = document.createElement("div");
             cell.className = "cell";
-            cell.style.height = cellSize + "px";
-            cell.style.width = cellSize + "px";
-            cell.style.backgroundSize = cellSize + "px";
+            //cell.style.height = cellSize + "px";
+            //cell.style.width = cellSize + "px";
+            //cell.style.backgroundSize = cellSize + "px";
             cell.id = "cell_" + x + y; // The id also acts as the cell coordinates
             cell.style.backgroundImage="url(graphics/blank.svg)";
             cell.setAttribute("onmouseover", "hover(" + x + y + ")");
@@ -28,25 +28,26 @@ function drawBoard() {
     }
 }
 
-function StartGame() {
+function startGame() {
     let keyIterator = cellMonitor.keys(); // We iterate through each cell on the board...
     for (let i = 0; i < totalTurns; i++) {
         cell = document.getElementById(keyIterator.next().value);
         cell.style.backgroundImage="url(graphics/start-up.svg)"; // ...to show a fancy pattern and then...
     }   
-    setTimeout(clear, 500); // ...to clear it for a new game
-    function clear() {
-        let keyIterator = cellMonitor.keys();
-        for (let i = 0; i < totalTurns; i++) {
-            let nextKey = keyIterator.next().value;
-            cell = document.getElementById(nextKey);
-            cell.style.backgroundImage="url(graphics/blank.svg)";
-            cellMonitor.set(nextKey, "blank");
-        }
-        gameState = true;
-        gameTurn = 1;
-        activePlayer = "x";
+    setTimeout(clearBoard, 500); // ...to clear it for a new game
+}
+
+function clearBoard() {
+    let keyIterator = cellMonitor.keys();
+    for (let i = 0; i < totalTurns; i++) {
+        let nextKey = keyIterator.next().value;
+        cell = document.getElementById(nextKey);
+        cell.style.backgroundImage="url(graphics/blank.svg)";
+        cellMonitor.set(nextKey, "blank");
     }
+    gameState = true;
+    gameTurn = 1;
+    activePlayer = "x";
 }
 
 function hover(id) { // Empty game squares are highlighted with the current player's symbol for clarity and prettyness
